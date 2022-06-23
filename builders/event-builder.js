1d788126796f2c5b00c3203eb95f568d544a9874
Rhian Lewis <rhianatwork@gmail.com>
2022-06-23 12:16:19 +0100

    Break long lines in event signature

--- builders/event-builder.js
+++ builders/event-builder.js
@@ -22,7 +22,11 @@ module.exports = {
     builder.push("\n");
     builder.push("```solidity");
     builder.push("\n");
-    builder.push(`event ${node.name}(${parameterList.join(", ")});`);
+    builder.push(`event ${node.name}(`);
+    parameterList.length > 1 ?
+    builder.push("\n" + parameterList.join("\n") + "\n") :
+    builder.push(parameterList.join(", "))
+    builder.push(")");
     builder.push("\n");
     builder.push("```");
     builder.push("\n");
@@ -33,4 +37,3 @@ module.exports = {
     return builder.join("");
   }
 };
-
